b72c973b44c0133cffa2beba46bced0a52828f1a
CalvinAndrean <calvinandrean456@gmail.com>
2022-11-01 08:28:44 +0700

    Guided Proximity

--- app/src/main/java/com/example/gd8_camera_d_10824/MainActivity.kt
+++ app/src/main/java/com/example/gd8_camera_d_10824/MainActivity.kt
@@ -1,37 +1,54 @@
 package com.example.gd8_camera_d_10824
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.annotation.SuppressLint
-import android.hardware.Camera
-import android.util.Log
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageButton
-//import android.content.Context
-//import android.hardware.Sensor
-//import android.hardware.SensorEvent
-//import android.hardware.SensorEventListener
-//import android.hardware.SensorManager
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var mCamera: Camera? = null
-    private var mCameraView: CameraView? = null
+    lateinit var sensorStatusTV: TextView
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try{
-            mCamera = Camera.open()
-        } catch(e: Exception){
-            Log.d("Error", "Failed to get Camera" + e.message)
+
+        sensorStatusTV = findViewById(R.id.idTVSensorStatus)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        if(proximitySensor == null){
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else{
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
         }
-        if(mCamera != null){
-            mCameraView = CameraView(this, mCamera!!)
-            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
-            camera_view.addView(mCameraView)
+
+    }
+
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener{
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int){
+            // method to check accuracy changed in sensor
         }
-        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
-            findViewById<View>(R.id.imgClose) as ImageButton
-            imageClose.setOnClickListener{view: View? -> System.exit(0)}
+
+        override fun onSensorChanged(event: SensorEvent){
+            if(event.sensor.type == Sensor.TYPE_PROXIMITY){
+                if(event.values[0] == 0f){
+                    sensorStatusTV.text="<<<Near>>>"
+                } else{
+                    sensorStatusTV.text="<<<<Away>>>> "
+                }
+            }
+        }
     }
 }
